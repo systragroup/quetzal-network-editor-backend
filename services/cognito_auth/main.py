e1ce831d84d4a2f5a41b1a363245815cbd513b07
Simon Boivin <sboivin@systra.com>
2024-12-18 10:52:09 -0500

    add NextToken to list users

--- services/cognito_auth/main.py
+++ services/cognito_auth/main.py
@@ -1,7 +1,6 @@
-
 import os
 from mangum import Mangum
-from fastapi import  FastAPI, HTTPException, Header, Request
+from fastapi import FastAPI, HTTPException, Header
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import boto3
@@ -11,15 +10,17 @@ from typing import Optional, Annotated
 
 from middlewares.exception import ExceptionHandlerMiddleware
 
+
 class User(BaseModel):
-    username: str
-    given_name:Optional[str]=None
-    family_name:Optional[str]=None
-    email: Optional[str]=None
-    password: str
+	username: str
+	given_name: Optional[str] = None
+	family_name: Optional[str] = None
+	email: Optional[str] = None
+	password: str
+
 
 class Username(BaseModel):
-    username: str
+	username: str
 
 
 USER_POOL_ID = os.environ['USER_POOL_ID']
@@ -27,201 +28,185 @@ USER_POOL_ID = os.environ['USER_POOL_ID']
 client = boto3.client('cognito-idp')
 
 
-
 app = FastAPI()
 # this is for AXIOS in vueJS (local dev)
-origins = ['http://localhost:8081', 
-           'https://localhost:8081', 
-           'https://systragroup.github.io/quetzal-network-editor-dev/', 
-           'https://systragroup.github.io',
-           'https://systragroup.github.io/quetzal-network-editor/']
+origins = [
+	'http://localhost:8081',
+	'https://localhost:8081',
+	'https://systragroup.github.io/quetzal-network-editor-dev/',
+	'https://systragroup.github.io',
+	'https://systragroup.github.io/quetzal-network-editor/',
+]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 app.add_middleware(ExceptionHandlerMiddleware)
 
-@app.get("/")
+
+@app.get('/')
 def read_root():
-    return {"Hello": "Quenedi Cognito API"}
+	return {'Hello': 'Quenedi Cognito API'}
 
-@app.get("/buckets/")
+
+@app.get('/buckets/')
 async def get_buckets(Authorization: Annotated[str | None, Header()] = None):
-    claims = auth(Authorization)
-    try:
-        role_arn = claims['cognito:roles'][0]
-    except Exception as err:
-        raise HTTPException(status_code=400,detail='User has no Cognito role')
-    role_name = role_arn.split('/')[-1]
-    try:
-        policies = get_policies_from_role(role_name)
-    except Exception as e:
-        raise Exception('error listing role policies:', e)
-    try:
-        inline_policies = get_inline_policies_from_role(role_name)
-        policies.extend(inline_policies)
-    except Exception as e:
-        raise Exception('error listing role inline policies:', e)
+	claims = auth(Authorization)
+	try:
+		role_arn = claims['cognito:roles'][0]
+	except Exception:
+		raise HTTPException(status_code=400, detail='User has no Cognito role')
+	role_name = role_arn.split('/')[-1]
+	try:
+		policies = get_policies_from_role(role_name)
+	except Exception as e:
+		raise Exception('error listing role policies:', e)
+	try:
+		inline_policies = get_inline_policies_from_role(role_name)
+		policies.extend(inline_policies)
+	except Exception as e:
+		raise Exception('error listing role inline policies:', e)
+
+	# get a list of all s3 buckets access ['arn:aws:s3:::quetzal-test/*, ...]
+	s3_policies = []
+	for policy in policies:
+		if policy[1]['Effect'] == 'Allow':
+			if type(policy[1]['Resource']) == list:
+				s3_policies = s3_policies + policy[1]['Resource']
+			else:
+				s3_policies.append(policy[1]['Resource'])
+	# remove arn:aws:s3::: and /*
+	buckets = [pol[13:-2] for pol in s3_policies]
+	buckets = [bucket for bucket in buckets if bucket not in ['quenedi-osm', 'quetzal-config', 'quetzal-api-bucket']]
+	buckets.sort()
+	return buckets
 
 
-    # get a list of all s3 buckets access ['arn:aws:s3:::quetzal-test/*, ...]
-    s3_policies = []
-    for policy in policies:
-        if (policy[1]['Effect'] == 'Allow'):
-            if type(policy[1]['Resource']) == list:
-                s3_policies = s3_policies + policy[1]['Resource']
-            else:
-                s3_policies.append(policy[1]['Resource'])
-    #remove arn:aws:s3::: and /*
-    buckets = [pol[13:-2] for pol in s3_policies]
-    buckets = [bucket for bucket in buckets if bucket not in ['quenedi-osm','quetzal-config','quetzal-api-bucket']]
-    buckets.sort()
-    return buckets
-
-
-@app.get("/listGroups/")
+@app.get('/listGroups/')
 async def list_groups(Authorization: Annotated[str | None, Header()] = None):
-    claims = auth(Authorization)
-    user_group = claims['cognito:groups'][0]
-    if user_group == 'admin':
-        response = client.list_groups(UserPoolId=USER_POOL_ID)
-        return [group['GroupName'] for group in response['Groups']]
-    else:
-        return [user_group]
-
-@app.get("/listUser/{group}/")
-async def list_users(group:str, Authorization: Annotated[str | None, Header()] = None):
-    claims = auth(Authorization)
-    user_group = claims['cognito:groups'][0]
-    if (user_group != group) and (user_group != 'admin'):
-        raise HTTPException(status_code=401,detail='not allowed')
-
-    response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=group)
-    users=[]
-    for user in response['Users']:
-        attrs_dict = {attr['Name']:attr['Value'] for attr in user['Attributes']}
-        user.update(attrs_dict)
-        user.pop('Attributes',None)
-        users.append(user)
-    return users
+	claims = auth(Authorization)
+	user_group = claims['cognito:groups'][0]
+	if user_group == 'admin':
+		response = client.list_groups(UserPoolId=USER_POOL_ID)
+		return [group['GroupName'] for group in response['Groups']]
+	else:
+		return [user_group]
 
 
-@app.post("/setUserPassword/")
-async def set_password(payload:User,Authorization: Annotated[str | None, Header()] = None):
-    claims = auth(Authorization)
+@app.get('/listUser/{group}/')
+async def list_users(group: str, Authorization: Annotated[str | None, Header()] = None):
+	claims = auth(Authorization)
+	user_group = claims['cognito:groups'][0]
+	if (user_group != group) and (user_group != 'admin'):
+		raise HTTPException(status_code=401, detail='not allowed')
 
-    user_group = claims['cognito:groups'][0]
-    
-    response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=user_group)
-    users = [user['Username'] for user in response['Users']]
-    if (payload.username in users) or (user_group == 'admin'):
-        try:
-            response = client.admin_set_user_password(
-                UserPoolId=USER_POOL_ID,
-                Username=payload.username,
-                Password=payload.password,
-                Permanent=False
-            )
-        except Exception as err :
-            raise HTTPException(status_code=400,detail=str(err))
-    else:
-        raise HTTPException(status_code=401,detail='not allowed')
+	# Can be paginated with NextToken
+	response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=group)
+	temp_users = response['Users']
+	while 'NextToken' in response.keys():
+		nextToken = response['NextToken']
+		response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=group, NextToken=nextToken)
+		temp_users.extend(response['Users'])
 
-    return response
-
-@app.post("/createUser/{group}/")
-async def create_user(group:str, payload:User, Authorization: Annotated[str | None, Header()] = None):
-    print(payload)
-    print(group)
-    claims = auth(Authorization)
-    user_group = claims['cognito:groups'][0]
-    if (user_group != group) and (user_group != 'admin'):
-        raise HTTPException(status_code=401,detail='not allowed')
-    
-    try:
-        response = client.admin_create_user(
-            UserPoolId=USER_POOL_ID,
-            Username=payload.username,
-            UserAttributes=[
-                {
-                    'Name': 'email',
-                    'Value': payload.email
-                },
-                {
-                    'Name': 'email_verified',
-                    'Value':  'true'
-                },
-                {
-                    'Name': 'given_name',
-                    'Value': payload.given_name
-                },
-                {
-                    'Name': 'family_name',
-                    'Value': payload.family_name
-                },
-            ],
-
-            TemporaryPassword=payload.password,
-        )
-        try:
-            ## add user to group
-            response = client.admin_add_user_to_group(
-                UserPoolId=USER_POOL_ID,
-                Username=payload.username,
-                GroupName=group
-            )
-        except Exception as err :
-            raise HTTPException(status_code=400,detail=str(err))
-            
-    except Exception as err :
-        raise HTTPException(status_code=400,detail=str(err))
-    return response
+	users = []
+	for user in temp_users:
+		attrs_dict = {attr['Name']: attr['Value'] for attr in user['Attributes']}
+		user.update(attrs_dict)
+		user.pop('Attributes', None)
+		users.append(user)
+	users.sort(key=lambda x: x['Username'])
+	return users
 
 
-@app.post("/deleteUser/")
-async def delete_user(payload:Username, Authorization: Annotated[str | None, Header()] = None):
-    claims = auth(Authorization)
+@app.post('/setUserPassword/')
+async def set_password(payload: User, Authorization: Annotated[str | None, Header()] = None):
+	claims = auth(Authorization)
 
-    user_group = claims['cognito:groups'][0]
-    
-    response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=user_group)
-    users = [user['Username'] for user in response['Users']]
-    if (payload.username in users) or (user_group == 'admin'):
-        try:
-           response = client.admin_delete_user(
-                        UserPoolId=USER_POOL_ID,
-                        Username=payload.username
-            )
-        except Exception as err :
-            raise HTTPException(status_code=400,detail=str(err))
-    else:
-        raise HTTPException(status_code=401,detail='not allowed')
+	user_group = claims['cognito:groups'][0]
 
-    return response
+	response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=user_group)
+	users = [user['Username'] for user in response['Users']]
+	if (payload.username in users) or (user_group == 'admin'):
+		try:
+			response = client.admin_set_user_password(UserPoolId=USER_POOL_ID, Username=payload.username, Password=payload.password, Permanent=False)
+		except Exception as err:
+			raise HTTPException(status_code=400, detail=str(err))
+	else:
+		raise HTTPException(status_code=401, detail='not allowed')
+
+	return response
 
 
-@app.post("/model/running/{stateMachineArn}/{scenario_path_S3}/")
-async def list_groups(stateMachineArn: str, scenario_path_S3:str):
-    # this function check if a scenario_path_s3 is in the list of running execARN for a stateMachine
-    # running this in fastAPI as we dont want to expose every inputs of every Model.
-    import json
-    scenario_path_S3 = scenario_path_S3.strip('/')
-    sf_client = boto3.client("stepfunctions")
-    execution_list = sf_client.list_executions(stateMachineArn=stateMachineArn,statusFilter='RUNNING')['executions']
-    # return arn if its in the running list. else return ''
-    for execution in execution_list:
-        arn = execution['executionArn']
-        resp = sf_client.describe_execution(executionArn=arn)
-        scen = json.loads(resp['input'])['scenario_path_S3'].strip('/')
-        if scenario_path_S3 == scen:
-            return arn
-    else:
-        return ''
+@app.post('/createUser/{group}/')
+async def create_user(group: str, payload: User, Authorization: Annotated[str | None, Header()] = None):
+	print(payload)
+	print(group)
+	claims = auth(Authorization)
+	user_group = claims['cognito:groups'][0]
+	if (user_group != group) and (user_group != 'admin'):
+		raise HTTPException(status_code=401, detail='not allowed')
+
+	try:
+		response = client.admin_create_user(
+			UserPoolId=USER_POOL_ID,
+			Username=payload.username,
+			UserAttributes=[
+				{'Name': 'email', 'Value': payload.email},
+				{'Name': 'email_verified', 'Value': 'true'},
+				{'Name': 'given_name', 'Value': payload.given_name},
+				{'Name': 'family_name', 'Value': payload.family_name},
+			],
+			TemporaryPassword=payload.password,
+		)
+		try:
+			## add user to group
+			response = client.admin_add_user_to_group(UserPoolId=USER_POOL_ID, Username=payload.username, GroupName=group)
+		except Exception as err:
+			raise HTTPException(status_code=400, detail=str(err))
+
+	except Exception as err:
+		raise HTTPException(status_code=400, detail=str(err))
+	return response
+
+
+@app.post('/deleteUser/')
+async def delete_user(payload: Username, Authorization: Annotated[str | None, Header()] = None):
+	claims = auth(Authorization)
+
+	user_group = claims['cognito:groups'][0]
+
+	response = client.list_users_in_group(UserPoolId=USER_POOL_ID, GroupName=user_group)
+	users = [user['Username'] for user in response['Users']]
+	if (payload.username in users) or (user_group == 'admin'):
+		try:
+			response = client.admin_delete_user(UserPoolId=USER_POOL_ID, Username=payload.username)
+		except Exception as err:
+			raise HTTPException(status_code=400, detail=str(err))
+	else:
+		raise HTTPException(status_code=401, detail='not allowed')
+
+	return response
+
+
+@app.post('/model/running/{stateMachineArn}/{scenario_path_S3}/')
+async def list_groups(stateMachineArn: str, scenario_path_S3: str):
+	# this function check if a scenario_path_s3 is in the list of running execARN for a stateMachine
+	# running this in fastAPI as we dont want to expose every inputs of every Model.
+	import json
+
+	scenario_path_S3 = scenario_path_S3.strip('/')
+	sf_client = boto3.client('stepfunctions')
+	execution_list = sf_client.list_executions(stateMachineArn=stateMachineArn, statusFilter='RUNNING')['executions']
+	# return arn if its in the running list. else return ''
+	for execution in execution_list:
+		arn = execution['executionArn']
+		resp = sf_client.describe_execution(executionArn=arn)
+		scen = json.loads(resp['input'])['scenario_path_S3'].strip('/')
+		if scenario_path_S3 == scen:
+			return arn
+	else:
+		return ''
+
+
 # note: could be get /model/status/ and we return the status (not just running)
 # but this could be long  sf_client.list_executions return 100 last. we dont want to check everything:
 # we would need to describe every exec (long time!). we could go dynamoDB. maybe a function that sync it
