d2dee9901837786b4feae6db7c1011d85ec7ae48
Simon Boivin <sboivin@systra.com>
2025-06-18 11:32:17 -0400

    add iam policy to get lambda tag to the cognito api

--- infra/base/modules/lambda_cognito/datas.tf
+++ infra/base/modules/lambda_cognito/datas.tf
@@ -2,8 +2,8 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {} # data.aws_region.current.name
 data "aws_ecr_image" "latest" {
-  repository_name   = var.ecr_repo_name
-  most_recent       = true
+  repository_name = var.ecr_repo_name
+  most_recent     = true
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -21,98 +21,107 @@ data "aws_iam_policy_document" "assume_role" {
 
 # policy to log on cloudwatch. using the just created log group arn
 data "aws_iam_policy_document" "lambda_logging" {
-    version = "2012-10-17"
-    statement   {
-        effect = "Allow"
-        actions = ["logs:CreateLogGroup"]
-        resources = [
-            "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
-            ]
-    }
-    statement {
-        effect = "Allow"
-        actions = [
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-        ]
-        resources = [
-            "${aws_cloudwatch_log_group.log_group.arn}:*"
-        ]
-    }
+  version = "2012-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["logs:CreateLogGroup"]
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.log_group.arn}:*"
+    ]
+  }
 }
 
 # policy to read and write on the s3 bucket
 data "aws_iam_policy_document" "iam_policy" {
-    version = "2012-10-17"
-	statement	{
-        effect= "Allow"
-        actions= [
-            "iam:GetPolicyVersion",
-			"iam:GetPolicy",
-            "iam:GetRolePolicy",
-			"iam:ListAttachedRolePolicies",
-            "iam:ListRolePolicies"
-        ]
-        resources= ["*"]
-    }
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetPolicyVersion",
+      "iam:GetPolicy",
+      "iam:GetRolePolicy",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListRolePolicies"
+    ]
+    resources = ["*"]
+  }
 }
 
 
 # policy to read and write on the s3 bucket
 data "aws_iam_policy_document" "cognito_policy" {
-    version = "2012-10-17"
-	statement	{
-        effect= "Allow"
-        actions= [
-            "cognito-idp:ForgotPassword",
-            "cognito-idp:GlobalSignOut",
-            "cognito-idp:ConfirmSignUp",
-            "cognito-idp:CreateUserPool",
-            "cognito-idp:ForgetDevice",
-            "cognito-idp:RevokeToken",
-            "cognito-idp:GetUserAttributeVerificationCode",
-            "cognito-idp:InitiateAuth",
-            "cognito-idp:DeleteUser",
-            "cognito-idp:SetUserMFAPreference",
-            "cognito-idp:GetUser",
-            "cognito-idp:ConfirmForgotPassword",
-            "cognito-idp:SetUserSettings",
-            "cognito-idp:SignUp",
-            "cognito-idp:VerifyUserAttribute",
-            "cognito-idp:ListDevices",
-            "cognito-idp:AdminSetUserPassword",
-            "cognito-idp:ListUserPools",
-            "cognito-idp:AssociateSoftwareToken",
-            "cognito-idp:VerifySoftwareToken",
-            "cognito-idp:GetDevice",
-            "cognito-idp:RespondToAuthChallenge",
-            "cognito-idp:DeleteUserAttributes",
-            "cognito-idp:UpdateUserAttributes",
-            "cognito-idp:DescribeUserPoolDomain",
-            "cognito-idp:UpdateDeviceStatus",
-            "cognito-idp:ChangePassword",
-            "cognito-idp:ConfirmDevice",
-            "cognito-idp:ResendConfirmationCode"
-        ]
-        resources= ["*"]
-    }
-    statement	{
-        effect= "Allow"
-        actions= ["cognito-idp:*"]
-        resources= ["arn:aws:cognito-idp:*:142023388927:userpool/*"]
-    }
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "cognito-idp:ForgotPassword",
+      "cognito-idp:GlobalSignOut",
+      "cognito-idp:ConfirmSignUp",
+      "cognito-idp:CreateUserPool",
+      "cognito-idp:ForgetDevice",
+      "cognito-idp:RevokeToken",
+      "cognito-idp:GetUserAttributeVerificationCode",
+      "cognito-idp:InitiateAuth",
+      "cognito-idp:DeleteUser",
+      "cognito-idp:SetUserMFAPreference",
+      "cognito-idp:GetUser",
+      "cognito-idp:ConfirmForgotPassword",
+      "cognito-idp:SetUserSettings",
+      "cognito-idp:SignUp",
+      "cognito-idp:VerifyUserAttribute",
+      "cognito-idp:ListDevices",
+      "cognito-idp:AdminSetUserPassword",
+      "cognito-idp:ListUserPools",
+      "cognito-idp:AssociateSoftwareToken",
+      "cognito-idp:VerifySoftwareToken",
+      "cognito-idp:GetDevice",
+      "cognito-idp:RespondToAuthChallenge",
+      "cognito-idp:DeleteUserAttributes",
+      "cognito-idp:UpdateUserAttributes",
+      "cognito-idp:DescribeUserPoolDomain",
+      "cognito-idp:UpdateDeviceStatus",
+      "cognito-idp:ChangePassword",
+      "cognito-idp:ConfirmDevice",
+      "cognito-idp:ResendConfirmationCode"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["cognito-idp:*"]
+    resources = ["arn:aws:cognito-idp:*:142023388927:userpool/*"]
+  }
 }
 
 
 # policy to read and write on the s3 bucket
 data "aws_iam_policy_document" "sfn_policy" {
-    version = "2012-10-17"
-	statement	{
-        effect= "Allow"
-        actions= [
-            "states:DescribeExecution",
-            "states:ListExecutions"
-            ]
-        resources= ["*"]
-    }
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "states:DescribeExecution",
+      "states:ListExecutions"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["lambda:GetFunctionConfiguration"]
+    resources = ["*"]
+  }
 }
--- infra/base/modules/lambda_cognito/lambda.tf
+++ infra/base/modules/lambda_cognito/lambda.tf
@@ -28,53 +28,60 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 
 # 5) create inline policy to access iam policies
 resource "aws_iam_role_policy" "iam_policy" {
-  name = "IAMLISTREAD_${var.function_name}"
-  role = aws_iam_role.iam_for_lambda.name
+  name   = "IAMLISTREAD_${var.function_name}"
+  role   = aws_iam_role.iam_for_lambda.name
   policy = data.aws_iam_policy_document.iam_policy.json
 }
 
 # 6) create inline policy with cognito admin role
 resource "aws_iam_role_policy" "cognito_policy" {
-  name = "COGNITOADMIN${var.function_name}"
-  role = aws_iam_role.iam_for_lambda.name
+  name   = "COGNITOADMIN${var.function_name}"
+  role   = aws_iam_role.iam_for_lambda.name
   policy = data.aws_iam_policy_document.cognito_policy.json
 }
 
 # 7) create inline policy with cognito admin role
 resource "aws_iam_role_policy" "sfn_policy" {
-  name = "SFNADMIN${var.function_name}"
-  role = aws_iam_role.iam_for_lambda.name
+  name   = "SFNADMIN${var.function_name}"
+  role   = aws_iam_role.iam_for_lambda.name
   policy = data.aws_iam_policy_document.sfn_policy.json
+}
+
+# 8) create inline policy for reading env on lambda
+resource "aws_iam_role_policy" "lambda_read_policy" {
+  name   = "LAMBDAREAD${var.function_name}"
+  role   = aws_iam_role.iam_for_lambda.name
+  policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
 
 # 6) create the Lambda function with dummy image from ECR
 resource "aws_lambda_function" "cognito_lambda" {
-    # If the file is not in the current working directory you will need to include a
-    # path.module in the filename.
-    function_name       = var.function_name
-    tags                = var.tags
-    role                = aws_iam_role.iam_for_lambda.arn
-    architectures       = ["x86_64"] 
-    package_type        = "Image"
-    image_uri           = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.ecr_repo_name}:${data.aws_ecr_image.latest.image_tags[0]}"
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  function_name = var.function_name
+  tags          = var.tags
+  role          = aws_iam_role.iam_for_lambda.arn
+  architectures = ["x86_64"]
+  package_type  = "Image"
+  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.ecr_repo_name}:${data.aws_ecr_image.latest.image_tags[0]}"
 
-    memory_size         = var.memory_size
-    timeout             = var.time_limit
-    ephemeral_storage {
-        size            = var.storage_size
-    }
-    environment {
-        variables = {
-            APP_CLIENT_ID = var.app_client_id,
-            REGION        = var.region
-            USER_POOL_ID  = var.user_pool_id
+  memory_size = var.memory_size
+  timeout     = var.time_limit
+  ephemeral_storage {
+    size = var.storage_size
+  }
+  environment {
+    variables = {
+      APP_CLIENT_ID = var.app_client_id,
+      REGION        = var.region
+      USER_POOL_ID  = var.user_pool_id
     }
   }
 
 
-    depends_on = [
-            aws_iam_role_policy_attachment.lambda_logs,
-            aws_cloudwatch_log_group.log_group,
-    ]
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.log_group,
+  ]
 }
