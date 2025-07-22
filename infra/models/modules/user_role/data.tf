4216192960753ce4a964873f094b78ac29d490e8
Simon Boivin <sboivin@systra.com>
2025-07-22 14:53:19 -0400

    clean a comments

--- infra/models/modules/user_role/data.tf
+++ infra/models/modules/user_role/data.tf
@@ -1,3 +1,4 @@
+# this policy is already created and attached. give acces to quetzal-api-bucket"
 data "aws_iam_policy" "config" {
   name = "s3_read_quetzal_config"
 }
@@ -14,16 +15,16 @@ data "aws_iam_policy_document" "assume_role" {
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
-    condition{
-      test = "ForAnyValue:StringEquals"
+    condition {
+      test     = "ForAnyValue:StringEquals"
       variable = "cognito-identity.amazonaws.com:aud"
-      values = [var.cognito_identity_pool_id]
+      values   = [var.cognito_identity_pool_id]
     }
 
-    condition{
-      test = "ForAnyValue:StringLike"
+    condition {
+      test     = "ForAnyValue:StringLike"
       variable = "cognito-identity.amazonaws.com:amr"
-      values = ["authenticated"]
+      values   = ["authenticated"]
     }
   }
 }
@@ -31,29 +32,29 @@ data "aws_iam_policy_document" "assume_role" {
 
 # policy to read and write on the s3 bucket
 data "aws_iam_policy_document" "user_s3_policy" {
-    version = "2012-10-17"
-	statement	{
-        effect= "Allow"
-        actions= ["s3:ListBucket"]
-        resources= ["arn:aws:s3:::${var.bucket_name}"]
-    }
-	statement	{
-        effect= "Allow"
-        actions= [
-            "s3:GetObject",
-            "s3:PutObject",
-            "s3:DeleteObject"
-        ]
-        resources= ["arn:aws:s3:::${var.bucket_name}/*"]
-    }
-	statement	{
-        effect= "Deny"
-        actions= [
-            "s3:PutObject",
-            "s3:DeleteObject"
-        ]
-        resources= ["arn:aws:s3:::${var.bucket_name}/base/*"]
-    }
-    
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.bucket_name}"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["arn:aws:s3:::${var.bucket_name}/*"]
+  }
+  statement {
+    effect = "Deny"
+    actions = [
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["arn:aws:s3:::${var.bucket_name}/base/*"]
+  }
+
 }
 
