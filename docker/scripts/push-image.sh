1c64f5fb68757db218cc3fa41e9882664963ac98
Simon Boivin <sboivin@systra.com>
2026-03-03 10:50:35 -0500

    update linux script to push lambda tags

--- docker/scripts/push-image.sh
+++ docker/scripts/push-image.sh
@@ -41,6 +41,7 @@ echo "updating lambda function ..."
 
 aws lambda wait function-updated --region $aws_region --function-name $AWS_LAMBDA_FUNCTION_NAME
 
+echo "updating lambda Tags ..."
 # Update Lamdba configuration to set tag  
 # Get current environment variables
 existing_env=$(aws lambda get-function-configuration \
@@ -48,13 +49,11 @@ existing_env=$(aws lambda get-function-configuration \
   --query 'Environment.Variables' \
   --output json)
 
-# Fallback to empty object if null
-existing_env=${existing_env:-{}}
-
-updated_env=$(echo "$existing_env" | jq -c --arg TAG "$TAG" '. + {IMAGE_TAG: $TAG}')
+# update env with new tag
+updated_env=$(jq -c --arg TAG "$TAG" '. // {} | .IMAGE_TAG = $TAG' <<< "$existing_env")
 
 aws lambda update-function-configuration \
-  --cli-input-json "$(jq -n --arg fn "$AWS_LAMBDA_FUNCTION_NAME" --argjson vars "$updated_env" \
-    '{FunctionName: $fn, Environment: {Variables: $vars}}')" > /dev/null
+   --cli-input-json "$(jq -n --arg fn "$AWS_LAMBDA_FUNCTION_NAME" --argjson vars "$updated_env" \
+     '{FunctionName: $fn, Environment: {Variables: $vars}}')" > /dev/null
 
 echo "success"
